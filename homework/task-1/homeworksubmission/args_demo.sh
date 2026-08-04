ddffc7e6bf1f9eed576766461230666d6b9987c4
user.email <ibnahsrf600@gmail.com>
2026-08-04 10:43:29 -0500

    Complete command-line arguments

---
+++ homework/task-1/homeworksubmission/args_demo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ # Tilte: command-line argument 
+ # Purpose: learning command-line arguments.
+
+    echo "First Argument: $1"
+    echo "Second Argument: $2"
+    echo "Third Argument: $3"
+    echo "Total Argument: $#"
